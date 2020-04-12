b4c1538ca6777ef6071a32e88d69b15ed2c82fa0
furutayo <63533143+furutayo@users.noreply.github.com>
2020-04-12 21:44:55 +0900

    Add files via upload

---
+++ indx.js
@@ -0,0 +1,9 @@
+document.getElementById("baz")
+        addEventListener("click",function()
+        {
+            alert('大切なことを報告します。');
+            alert('留学することになりました。');
+            alert('...');
+            alert('うっそーーーー！')
+
+        });
